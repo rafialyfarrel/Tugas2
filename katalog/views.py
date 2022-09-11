a2c70e08000c6446842823ca4903e9cec67ce908
rafialyfarrel <muhammad.rafialy@ui.ac.id>
2022-09-11 17:57:21 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_barang_catalog,
+        'nama' : 'Muhammad Rafialy Farrel',
+        'id' : '2106751171',
+    }
+    return render(request, "katalog.html", context)
